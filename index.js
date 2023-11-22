43e227b701e45343f0fad15a116b6f2a26f0cd63
krathus88 <marcoribeiro4@hotmail.com>
2023-11-22 15:21:03 +0000

    Add Top Anime Slider

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://api.jikan.moe/v4/";
+const API_URL = "https://api.jikan.moe/v4";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,11 +12,18 @@ app.use(express.static("public"));
 const animeName = ["ONE PIECE", "JUJUTSU KAISEN"];
 const animeInfo = ["Some representative placeholder content for the first lide of the carousel", "Some representative placeholder content for the first lide of the carousel"];
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", {
-        animeName,
-        animeInfo,
-    });
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get(API_URL + "/top/anime");
+        res.render("index.ejs", {
+            anime: result.data.data,
+            animeName,
+            animeInfo,
+        })
+    } catch(error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
 });
 
 app.listen(port, () => {
